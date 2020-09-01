b3d995030b0d51cce74cc504b56e4cb2c6081b9e
Zveta <svet.v.gorbunova@gmail.com>
2020-09-01 19:54:48 +0300

    Updates

---
+++ precache-manifest.b7b5ff2a552015fa78fd4d24ee8c9c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f6dee9b0c4c8aff5d44fe9c1b0af70",
+    "url": "/news-portal-react/index.html"
+  },
+  {
+    "revision": "d737c8482040553ffe64",
+    "url": "/news-portal-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4eb0a9818ffeb94d2cc",
+    "url": "/news-portal-react/static/js/2.f62d12d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-portal-react/static/js/2.f62d12d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d737c8482040553ffe64",
+    "url": "/news-portal-react/static/js/main.9a56a9a6.chunk.js"
+  },
+  {
+    "revision": "cbc4230697ab5724047f",
+    "url": "/news-portal-react/static/js/runtime-main.320ebbed.js"
+  }
+]);
